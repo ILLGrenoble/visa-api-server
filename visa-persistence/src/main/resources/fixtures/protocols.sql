620442d5ae70f8d41262540349c7b83abd5683c3
Stuart Caunt <caunt@ill.fr>
2023-02-01 10:29:07 +0100

    Use webx-relay jar from maven central. Add webx to protocol fitures.

--- visa-persistence/src/main/resources/fixtures/protocols.sql
+++ visa-persistence/src/main/resources/fixtures/protocols.sql
@@ -1,3 +1,4 @@
-INSERT INTO protocol (name, port) VALUES ('RDP', 3389);
-INSERT INTO protocol (name, port) VALUES ('GUACD', 4822);
-INSERT INTO protocol (name, port) VALUES ('JUPYTER', 8888);
+INSERT INTO protocol (name, port, optional) VALUES ('RDP', 3389, true);
+INSERT INTO protocol (name, port, optional) VALUES ('GUACD', 4822, false);
+INSERT INTO protocol (name, port, optional) VALUES ('JUPYTER', 8888, true);
+INSERT INTO protocol (name, port, optional) VALUES ('WEBX', 5555, true);
