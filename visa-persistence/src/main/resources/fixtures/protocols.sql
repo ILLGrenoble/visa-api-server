fc65ff8602c4318a731150097d4a743395b34aa9
Stuart Caunt <caunt@ill.fr>
2023-11-14 12:09:50 +0100

    Add VISAFS protocol to fixtures.

--- visa-persistence/src/main/resources/fixtures/protocols.sql
+++ visa-persistence/src/main/resources/fixtures/protocols.sql
@@ -2,3 +2,4 @@ INSERT INTO protocol (name, port, optional) VALUES ('RDP', 3389, true);
 INSERT INTO protocol (name, port, optional) VALUES ('GUACD', 4822, false);
 INSERT INTO protocol (name, port, optional) VALUES ('JUPYTER', 8888, true);
 INSERT INTO protocol (name, port, optional) VALUES ('WEBX', 5555, true);
+INSERT INTO protocol (name, port, optional) VALUES ('VISAFS', 8090, true);
