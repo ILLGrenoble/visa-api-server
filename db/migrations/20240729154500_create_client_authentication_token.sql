366782aa4a63f8fa7e53c4f439ffa3a79f3d4618
Stuart Caunt <caunt@ill.fr>
2024-07-29 15:52:12 +0200

    Add migration to create table for client authentication token (postgres should be version 12 or above now to be compatible with hibernate).

---
+++ db/migrations/20240729154500_create_client_authentication_token.sql
@@ -0,0 +1,14 @@
+-- migrate:up
+
+CREATE SEQUENCE client_authentication_token_id_seq;
+CREATE TABLE client_authentication_token (
+     id             BIGINT NOT NULL CONSTRAINT client_authentication_token_pk PRIMARY KEY DEFAULT nextval('client_authentication_token_id_seq'),
+     created_at     TIMESTAMP NOT NULL,
+     updated_at     TIMESTAMP NOT NULL,
+     client_id      VARCHAR(250) NOT NULL,
+     token          VARCHAR(250) NOT NULL,
+     user_id        VARCHAR(255) NOT NULL constraint fk_users_id references users
+);
+
+-- migrate:down
+
