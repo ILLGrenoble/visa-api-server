fafe1aeba39e2a1ee374302ad851be7de09639b5
Stuart Caunt <caunt@ill.fr>
2024-07-29 15:53:50 +0200

    modify the table creation script.

--- db/migrations/20240729154500_create_client_authentication_token.sql
+++ db/migrations/20240729154500_create_client_authentication_token.sql
@@ -1,7 +1,7 @@
 -- migrate:up
 
 CREATE SEQUENCE client_authentication_token_id_seq;
-CREATE TABLE client_authentication_token (
+CREATE TABLE IF NOT EXISTS client_authentication_token (
      id             BIGINT NOT NULL CONSTRAINT client_authentication_token_pk PRIMARY KEY DEFAULT nextval('client_authentication_token_id_seq'),
      created_at     TIMESTAMP NOT NULL,
      updated_at     TIMESTAMP NOT NULL,
