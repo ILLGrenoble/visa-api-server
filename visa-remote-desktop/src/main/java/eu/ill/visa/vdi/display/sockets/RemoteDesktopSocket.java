79f751ca86f61fa3bf22d1d01305dddf6198d23f
Stuart Caunt <caunt@ill.fr>
2025-04-17 12:17:56 +0200

    Redo locks and message queues: only lock/queue if waiting for completion of connection event.

--- visa-remote-desktop/src/main/java/eu/ill/visa/vdi/display/sockets/RemoteDesktopSocket.java
+++ visa-remote-desktop/src/main/java/eu/ill/visa/vdi/display/sockets/RemoteDesktopSocket.java
@@ -6,9 +6,7 @@ import eu.ill.visa.vdi.domain.models.SocketClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
@@ -26,15 +24,23 @@ public abstract class RemoteDesktopSocket {
         MESSAGE,
     }
 
-    private record WorkerEvent(SocketClient socketClient, WorkerHandler worker, WorkerEventType type) { }
+    private record WorkerEvent(SocketClient socketClient, WorkerHandler worker, WorkerEventType type) {
+        public void execute() {
+            try {
+                worker.doWork();
+
+            } catch (Exception error) {
+                logger.error("Remote Desktop Event ({}) failed with protocol {}: {}", type, socketClient.protocol(), error.getMessage());
+            }
+        }
+    }
 
     private static final Logger logger = LoggerFactory.getLogger(RemoteDesktopSocket.class);
 
     private RemoteDesktopConnectSubscriber connectSubscriber;
     private RemoteDesktopDisconnectSubscriber disconnectSubscriber;
 
-    private final ConcurrentHashMap<String, Object> sessionLocks = new ConcurrentHashMap<>();
-    private final Map<String, LinkedList<WorkerEvent>> sessionEventQueues = new HashMap<>();
+    private final ConcurrentHashMap<String, LinkedList<WorkerEvent>> sessionQueues = new ConcurrentHashMap<>();
 
     private final Executor desktopEventExecutor;
 
@@ -50,66 +56,77 @@ public abstract class RemoteDesktopSocket {
         this.disconnectSubscriber = disconnectSubscriber;
     }
 
-    private void queueEvent(final WorkerEvent workerEvent) {
-        Object lock = sessionLocks.computeIfAbsent(workerEvent.socketClient().clientId(), id -> new Object());
-        synchronized (lock) {
-            // Check if lock still valid
-            if (sessionLocks.containsKey(workerEvent.socketClient().clientId())) {
-                final LinkedList<WorkerEvent> sessionEventQueue = sessionEventQueues.computeIfAbsent(workerEvent.socketClient.clientId(), id -> new LinkedList<>());
-                sessionEventQueue.addLast(workerEvent);
+    private void handleEvent(final WorkerEvent workerEvent) {
+        final LinkedList<WorkerEvent> queue = sessionQueues.get(workerEvent.socketClient().clientId());
+        // If queue doesn't exist then send the event immediately
+        if (queue == null) {
+            this.asyncRunEvent(workerEvent);
 
-                // Run event immediately if it is the only one on the queue
-                if (sessionEventQueue.size() == 1) {
-                    this.runEvent(workerEvent);
+        } else {
+            // lock the queue
+            synchronized (queue) {
+                // Check if lock still valid (will be removed once the connect event has finished and the queue is empty)
+                if (sessionQueues.containsKey(workerEvent.socketClient().clientId())) {
+                    // Add event to the queue
+                    queue.addLast(workerEvent);
+
+                    // Run event immediately if it is the only one on the queue
+                    if (queue.size() == 1) {
+                        this.asyncRunQueuedEvent(workerEvent);
+                    }
+
+                } else {
+                    this.asyncRunEvent(workerEvent);
                 }
             }
         }
+
     }
 
     private void onWorkerEventTerminated(final WorkerEvent event) {
-        Object lock = sessionLocks.computeIfAbsent(event.socketClient().clientId(), id -> new Object());
-        synchronized (lock) {
-            final LinkedList<WorkerEvent> sessionEventQueue = sessionEventQueues.get(event.socketClient().clientId());
-            sessionEventQueue.removeFirst();
+        final LinkedList<WorkerEvent> queue = sessionQueues.get(event.socketClient().clientId());
+        if (queue == null) {
+            return;
+        }
 
-            if (event.type().equals(WorkerEventType.DISCONNECT)) {
-                sessionLocks.remove(event.socketClient().clientId());
-                sessionEventQueues.remove(event.socketClient().clientId());
+        synchronized (queue) {
+            queue.removeFirst();
+
+            if (queue.isEmpty()) {
+                sessionQueues.remove(event.socketClient().clientId());
 
             } else {
-                if (!sessionEventQueue.isEmpty()) {
-                    final WorkerEvent nextWorkerEvent = sessionEventQueue.getFirst();
-                    this.runEvent(nextWorkerEvent);
-                }
+                final WorkerEvent nextWorkerEvent = queue.getFirst();
+                this.asyncRunQueuedEvent(nextWorkerEvent);
             }
         }
     }
 
-    private void runEvent(final WorkerEvent event) {
+    private void asyncRunEvent(final WorkerEvent event) {
+        // Execute handler on virtual thread to allow JTA transactions
+        CompletableFuture.runAsync(event::execute, desktopEventExecutor);
+    }
+
+    private void asyncRunQueuedEvent(final WorkerEvent event) {
         // Execute handler on virtual thread to allow JTA transactions
         CompletableFuture.runAsync(() -> {
-            try {
-                event.worker().doWork();
-
-            } catch (Exception error) {
-                logger.error("Remote Desktop Event ({}) failed with protocol {}: {}", event.type(), event.socketClient().protocol(), error.getMessage());
-            }
-
+            event.execute();
             this.onWorkerEventTerminated(event);
 
         }, desktopEventExecutor);
     }
 
     protected void onOpen(final SocketClient socketClient) {
-        this.queueEvent(new WorkerEvent(socketClient, () -> this.connectSubscriber.onConnect(socketClient, this::sendNop), WorkerEventType.CONNECT));
+        sessionQueues.put(socketClient.clientId(), new LinkedList<>());
+        this.handleEvent(new WorkerEvent(socketClient, () -> this.connectSubscriber.onConnect(socketClient, this::sendNop), WorkerEventType.CONNECT));
     }
 
     protected void onClose(final SocketClient socketClient) {
-        this.queueEvent(new WorkerEvent(socketClient, () -> this.disconnectSubscriber.onDisconnect(socketClient), WorkerEventType.DISCONNECT));
+        this.handleEvent(new WorkerEvent(socketClient, () -> this.disconnectSubscriber.onDisconnect(socketClient), WorkerEventType.DISCONNECT));
     }
 
     protected void onMessage(final SocketClient socketClient, WorkerHandler handler) {
-        this.queueEvent(new WorkerEvent(socketClient, handler, WorkerEventType.MESSAGE));
+        this.handleEvent(new WorkerEvent(socketClient, handler, WorkerEventType.MESSAGE));
     }
 
     protected abstract void sendNop(final SocketClient socketClient);
