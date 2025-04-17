a4b6dba0e5eafcce98cc84be2e0cc421883a4a1d
Stuart Caunt <caunt@ill.fr>
2025-04-17 14:21:20 +0200

    Bug fix: corrected position in buffer array to obtain instruction type.

--- visa-remote-desktop/src/main/java/eu/ill/visa/vdi/display/subscribers/WebXRemoteDesktopEventSubscriber.java
+++ visa-remote-desktop/src/main/java/eu/ill/visa/vdi/display/subscribers/WebXRemoteDesktopEventSubscriber.java
@@ -4,12 +4,17 @@ import eu.ill.visa.business.services.InstanceService;
 import eu.ill.visa.core.entity.enumerations.InstanceActivityType;
 import eu.ill.visa.vdi.business.concurrency.ConnectionThread;
 import eu.ill.visa.vdi.business.services.DesktopSessionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.ByteBuffer;
 
 import static java.nio.ByteOrder.LITTLE_ENDIAN;
 
 public class WebXRemoteDesktopEventSubscriber extends RemoteDesktopEventSubscriber<byte[]> {
+    private static final Logger logger = LoggerFactory.getLogger(WebXRemoteDesktopEventSubscriber.class);
+
+    private static final int INSTRUCTION_TYPE_OFFSET = 20;
 
     public WebXRemoteDesktopEventSubscriber(final DesktopSessionService desktopSessionService,
                                             final InstanceService instanceService) {
@@ -19,7 +24,7 @@ public class WebXRemoteDesktopEventSubscriber extends RemoteDesktopEventSubscrib
     @Override
     protected InstanceActivityType getControlActivityType(byte[] data) {
 
-        ByteBuffer instructionWrapper = ByteBuffer.wrap(data, 16, 4).order(LITTLE_ENDIAN);
+        ByteBuffer instructionWrapper = ByteBuffer.wrap(data, INSTRUCTION_TYPE_OFFSET, 4).order(LITTLE_ENDIAN);
         int instructionType = instructionWrapper.getInt() & 0x000000ff;
 
         if (instructionType == 5) {
