b9b450a409ecac7c24f291fb563359e9a2f11638
Stuart Caunt <caunt@ill.fr>
2026-08-25 13:49:50 +0200

    Delete instances immediately (when using admin pages).

--- visa-web-graphql/src/main/java/eu/ill/visa/web/graphql/resources/InstanceResource.java
+++ visa-web-graphql/src/main/java/eu/ill/visa/web/graphql/resources/InstanceResource.java
@@ -229,13 +229,11 @@ public class InstanceResource {
             throw new EntityNotFoundException("Instance not found for the given id");
         }
 
-        if (instance.getComputeId() == null || instance.hasAnyState(List.of(InstanceState.STOPPED, InstanceState.ERROR, InstanceState.UNKNOWN, InstanceState.MIGRATING, InstanceState.UNAVAILABLE))) {
-            this.performInstanceAction(id, InstanceCommandType.DELETE);
-        } else {
-            instance.setDeleteRequested(true);
-            this.instanceService.save(instance);
-            this.performInstanceAction(id, InstanceCommandType.SHUTDOWN);
-        }
+        instance.setDeleteRequested(true);
+        this.instanceService.save(instance);
+
+        this.performInstanceAction(id, InstanceCommandType.DELETE);
+
         return new Message("Instance is scheduled for deletion");
     }
 
